1b69538ad8d23822c265e15542a5957467c5406d
Aimar0317 <aimar.garcia@alumni.mondragon.edu>
2025-03-10 13:04:39 +0100

    Loading changed to dashboard

--- app/DashboardPage/page.js
+++ app/DashboardPage/page.js
@@ -1,9 +1,6 @@
 // pages/dashboard.js
-'use client'
-import { Suspense } from 'react';
 import Dashboard from '../../components/dashboard/dashboard';  // Asegúrate de que la ruta sea correcta
 import Sidebar from '../../components/sidebar/sidebar';
-import Loading from '../../components/sidebar/loading';
 import styles from './dashboardPage.module.css';
 export default function DashboardPage() {
   return (
